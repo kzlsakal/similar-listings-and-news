48027433fce45135f23f4ddc8e27062374fbbf78
kzlsakal <dorukgurleyen@gmail.com>
2020-08-05 12:41:02 -0700

    Update webpack with public, client, server folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'public/src'),
+  entry: path.resolve(__dirname, 'client/index.jsx'),
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'similar-listings-news.bundle.js'
   },
   rules: [
     {
       test: /\.jsx$/,
       exclude: /node_modules/,
+      include: [path.resolve(__dirname, 'client')],
       use: {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     }
   ]
 };
