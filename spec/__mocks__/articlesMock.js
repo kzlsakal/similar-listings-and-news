b6814b0d9eb8482e34e335de9227283b9919ad66
kzlsakal <dorukgurleyen@gmail.com>
2020-08-11 16:12:07 -0700

    Add mock article API data to unit testing

---
+++ spec/__mocks__/articlesMock.js
@@ -0,0 +1,50 @@
+const response = {};
+
+response.data = [
+  {
+    'tags': [
+      'Bass'
+    ],
+    '_id': '5f3321f10313067d6be1cd4a',
+    'author': 'Lilliana Jennings',
+    'title': 'Enhanced Bass Internet Solution',
+    'type': 'tone report',
+    'imageSmall': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/small (3).jpg',
+    'imageFull': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/full (3).jpg',
+    'content': 'Blanditiis autem architecto natus nobis enim omnis autem. Praesentium est quaerat dignissimos. Facere tempora nostrum ut occaecati aut id harum. Totam dolorum nihil omnis voluptatum sit similique fugit consequatur.\n \rHic quaerat quam amet repellendus sit libero sequi nostrum. Ut ex et mollitia corrupti sit reiciendis aperiam veritatis consequatur. Qui occaecati optio perferendis animi in. Ullam accusantium ut et officia dicta porro nihil error. Doloribus autem unde repellendus accusamus doloribus dolores repellendus corrupti vero. Iste in molestiae.\n \rLaboriosam nemo error est vel et dolorem commodi. Est maiores sed eum explicabo dignissimos ut consequatur voluptatem corrupti. Praesentium minima voluptatem. Dignissimos qui aut consequatur amet at quidem voluptatem aut.',
+    'published': '2020-08-11T22:55:44.623Z',
+    '__v': 0
+  },
+  {
+    'tags': [
+      'Bass'
+    ],
+    '_id': '5f3321f10313067d6be1cd4f',
+    'author': 'Abel Andre',
+    'title': 'Visionary Bass Projection',
+    'type': 'news and reviews',
+    'imageSmall': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/small (8).jpg',
+    'imageFull': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/full (8).jpg',
+    'content': 'Ex eaque perspiciatis placeat neque eos incidunt ipsa. Et laboriosam dolores. Consequatur numquam eos dolor voluptate velit corporis quo laudantium.\n \rVeniam inventore corporis aut quasi id eos quibusdam qui reprehenderit. Porro sunt sed dolor magnam. Perferendis ex maxime. Iure explicabo cum dolor autem.\n \rEt et hic nihil. Vel omnis molestiae nostrum sed magnam sapiente sapiente placeat. Quis sint est debitis unde voluptatem esse voluptate. Pariatur neque dolorem in et et consequuntur. Ipsum unde dolores.',
+    'published': '2020-08-11T22:55:44.623Z',
+    '__v': 0
+  },
+  {
+    'tags': [
+      'Bass'
+    ],
+    '_id': '5f3321f10313067d6be1cd51',
+    'author': 'Allan Freeda',
+    'title': 'Object-based Bass Approach',
+    'type': 'news and reviews',
+    'imageSmall': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/small (10).jpg',
+    'imageFull': 'https://kizilsakal-dev.s3-us-west-2.amazonaws.com/fec-sln/articles/full (10).jpg',
+    'content': 'Voluptatem repudiandae fuga et deleniti illum cumque. Autem fugiat rerum quo reprehenderit quasi delectus voluptatem optio. Eligendi et aut dolorem pariatur illo dolor facere et.\n \rMagni dolorem officia consequatur quia eius totam ut. Officia impedit est omnis sint dicta dolore. Corporis laboriosam quidem facilis in incidunt occaecati. Aut quos unde voluptates expedita consequatur debitis ea ut quis. Voluptatem ducimus illo maxime provident. Sed inventore consequatur provident libero magni exercitationem sunt doloremque et.\n \rVoluptas possimus quidem. In culpa repellendus accusamus ratione. Quisquam temporibus expedita id ducimus est ut aut et ab.',
+    'published': '2020-08-11T22:55:44.624Z',
+    '__v': 0
+  }
+];
+
+response.json = () => response.data;
+
+module.exports = response;
--- spec/unit-testing/enzyme.setup.js
+++ spec/unit-testing/enzyme.setup.js
@@ -2,6 +2,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import listingMock from './../__mocks__/listingMock';
 import categoryMock from './../__mocks__/categoryMock';
+import articlesMock from './../__mocks__/articlesMock';
 
 configure({ adapter: new Adapter() });
 
@@ -12,6 +13,8 @@ document.body.appendChild(sln);
 global.fetch = jest.fn((input) => {
   if (input.indexOf('listings') > -1) {
     return Promise.resolve(categoryMock);
+  } else if (input.indexOf('news') > -1) {
+    return Promise.resolve(articlesMock);
   }
   return Promise.resolve(listingMock);
 });
