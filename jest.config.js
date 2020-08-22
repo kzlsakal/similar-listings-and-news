3bcfc6379a4814940a59f313f74b5b5ab2ab8d14
kzlsakal <dorukgurleyen@gmail.com>
2020-08-22 15:45:38 -0700

    Add unit tests for server

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
+    './server/sln.js',
     '!./server/db/*',
     './client/**/*.{js,jsx}'
   ],
